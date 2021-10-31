b92867a24ee8cb770afb3e50b7e0ea6d62d4d00c
KhalilBenKhoud <87437538+KhalilBenKhoud@users.noreply.github.com>
2021-10-31 13:01:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -36,75 +36,10 @@ async function getData()
       })
    }
 
-   //crew data
+   
 
   
 }
 
 getData()
 
-async function getDataCrew()
-{
-   var response = await fetch("../data.json")
-   var data = await response.json()
-
-
- var buttons = document.getElementsByClassName("buttons")[0]
- var role =  document.getElementsByClassName("role")[0]
- var name = document.getElementsByClassName("name")[0]
- var bio =  document.getElementsByClassName("bio")[0]
- var memberImg =  document.getElementById("Mimg")
-  
- for(let i=0 ; i< buttons.children.length ; i++)
-  {
-     buttons.children[i].addEventListener("click", (e) => {
-       console.log("clicked")
-       Array.from(buttons.children).forEach(element => {
-           element.style.backgroundColor = "grey"
-           
-        })
-
-        e.target.style.backgroundColor = "white"
-        role.innerHTML = data.crew[i].role
-        name.innerHTML = data.crew[i].name
-        bio.innerHTML = data.crew[i].bio
-        memberImg.src = data.crew[i].images.png
-     })
-
-  }
-
-}
-
-getDataCrew()
-
-async function getDataTech()
-{
-   var response = await fetch("../data.json")
-   var data = await response.json()
-
-   var Tbuttons = document.getElementsByClassName("techBtn")[0]
-   var Ttitle =  document.getElementsByClassName("Ttitle")[0]
-   var Tdescription = document.getElementsByClassName("Tdescription")[0]
-   var Timg = document.getElementById("Timg")
-    var landscape = document.getElementsByClassName("landscape")[0]
-   for(let i=0 ; i< Tbuttons.children.length ; i++)
-   {
-
-      Tbuttons.children[i].addEventListener("click", (e) => {
-      Array.from(Tbuttons.children).forEach(element => {
-         element.style.backgroundColor = "transparent"
-         element.style.color = "white"
-         })
-
-         e.target.style.backgroundColor = "white"
-         e.target.style.color = "black"
-         Ttitle.innerHTML = data.technology[i].name
-         Tdescription.innerHTML = data.technology[i].description
-         Timg.src = data.technology[i].images.portrait
-         landscape.src = data.technology[i].images.landscape
-   })
-
-   }
-}
-
-getDataTech()
